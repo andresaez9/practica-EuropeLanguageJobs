6d5527aa55ec5bdd3a873a5ead8996d16a6bea7c
Andres Saez <andres14.ass@gmail.com>
2024-03-01 02:19:29 +0100

    feat(Pinia): add pinia and use Store from dog form

--- frontend/src/components/DogForm.vue
+++ frontend/src/components/DogForm.vue
@@ -1,80 +1,35 @@
-<template>
-    <div class="container mx-auto py-8">
-        <h2 class="text-3xl font-semibold mb-8 text-center text-gray-800">Subir Foto y Detalles del Perro</h2>
-  
-        <input type="file" @change="handleFileUpload" class="mb-8 block mx-auto">
-  
-        <form @submit.prevent="submitForm" class="max-w-xl mx-auto">
-            <div class="mb-6">
-                <label for="breed" class="block text-lg font-medium text-gray-700">Raza:</label>
-                <input v-model="dogDetails.breed" type="text" id="breed" required class="mt-1 p-4 border border-gray-300 rounded-md w-full text-lg">
-            </div>
-            <div class="mb-6">
-                <label for="size" class="block text-lg font-medium text-gray-700">Tamaño:</label>
-                <input v-model="dogDetails.size" type="text" id="size" required class="mt-1 p-4 border border-gray-300 rounded-md w-full text-lg">
-            </div>
-            <div class="mb-6">
-                <label for="color" class="block text-lg font-medium text-gray-700">Color:</label>
-                <input v-model="dogDetails.color" type="text" id="color" required class="mt-1 p-4 border border-gray-300 rounded-md w-full text-lg">
-            </div>
-            <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md text-lg block mx-auto">
-                Guardar Detalles
-            </button>
-        </form>
-        <div v-if="errorMessage" class="text-red-600 text-center mt-4">{{ errorMessage }}</div>
-    </div>
-    <router-link to="/details" class="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-3 px-9 rounded-md text-lg block mx-auto w-60">Ver todos los perros</router-link>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const dogDetails = ref({
-    breed: '',
-    size: '',
-    color: '',
-    image: null
-  });
+<script setup>
+  import { useDogStore } from 'stores/dogStore';
 
-  const errorMessage = ref('');
-  
-  const handleFileUpload = (event) => {
-    dogDetails.value.image = event.target.files[0];
-  }
-  
-  async function submitForm() {
-    try {
-      const formData = new FormData();
-      formData.append('breed', dogDetails.value.breed);
-      formData.append('size', dogDetails.value.size);
-      formData.append('color', dogDetails.value.color);
-      formData.append('image', dogDetails.value.image);
-  
-      const response = await fetch('http://127.0.0.1:8000/api/dogs', {
-        method: 'POST',
-        body: formData
-      });
-
-      if (!response.ok) {
-        errorMessage.value = 'Error al subir la foto o detalles del perro, hay valores repetidos';
-        return;
-      }
-  
-      if (response.ok) {
-        const data = await response.json();
-        
-        dogDetails.value.breed = '';
-        dogDetails.value.size = '';
-        dogDetails.value.color = '';
-        dogDetails.value.image = null;
-
-        console.log('Respuesta del backend:', data);
-      } else {
-        console.error('Error en la petición al backend:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error en la petición al backend:', error);
-    }
-  }
+  const dogStore = useDogStore();
 </script>
+
+<template>
+  <div class="container mx-auto py-8">
+    <h2 class="text-3xl font-semibold mb-8 text-center text-gray-800">Subir Foto y Detalles del Perro</h2>
+
+    <input type="file" @change="dogStore.handleFileUpload" class="mb-8 block mx-auto">
+
+    <form @submit.prevent="dogStore.submitForm()" class="max-w-xl mx-auto">
+      <div class="mb-6">
+        <label for="breed" class="block text-lg font-medium text-gray-700">Raza:</label>
+        <input v-model="dogStore.dogsDetails.breed" type="text" id="breed" required class="mt-1 p-4 border border-gray-300 rounded-md w-full text-lg">
+      </div>
+      <div class="mb-6">
+        <label for="size" class="block text-lg font-medium text-gray-700">Tamaño:</label>
+        <input v-model="dogStore.dogsDetails.size" type="text" id="size" required class="mt-1 p-4 border border-gray-300 rounded-md w-full text-lg">
+      </div>
+      <div class="mb-6">
+        <label for="color" class="block text-lg font-medium text-gray-700">Color:</label>
+        <input v-model="dogStore.dogsDetails.color" type="text" id="color" required class="mt-1 p-4 border border-gray-300 rounded-md w-full text-lg">
+      </div>
+      <button type="submit" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md text-lg block mx-auto">
+        Guardar Detalles
+      </button>
+    </form>
+    <div v-if="dogStore.errorMessage" class="text-red-600 text-center mt-4">{{ dogStore.errorMessage }}</div>
+    <div v-if="dogStore.checkMessage" class="text-green-600 text-center mt-4">{{ dogStore.checkMessage }}</div>
+  </div>
+  <router-link to="/details" class="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-3 px-9 rounded-md text-lg block mx-auto w-60">Ver todos los perros</router-link>
+</template>
   
---
+++ frontend/src/stores/dogStore.js
@@ -0,0 +1,67 @@
+import { defineStore } from "pinia";
+
+export const useDogStore = defineStore('dogStore', {
+    state: () => ({
+        dogsDetails: {
+            breed: "",
+            size: "",
+            color: "",
+            image: null,
+        },
+
+        errorMessage: "",
+        checkMessage: "",
+    }),
+
+    getters: {
+        getDogsDetails() {
+            return this.dogsDetails;
+        },
+
+        getErrorMessage() {
+            return this.errorMessage;
+        },
+
+        getCheckMessage() {
+            return this.checkMessage;
+        }
+    },
+
+    actions: {
+        handleFileUpload(event) {
+            this.dogsDetails.image = event.target.files[0];
+        },
+
+        async submitForm() {
+          try {
+            const formData = new FormData();
+            formData.append('breed', this.dogsDetails.breed);
+            formData.append('size', this.dogsDetails.size);
+            formData.append('color', this.dogsDetails.color);
+            formData.append('image', this.dogsDetails.image);
+    
+            const response = await fetch('http://127.0.0.1:8000/api/dogs', {
+              method: 'POST',
+              body: formData
+            });
+    
+            if (!response.ok) {
+              this.errorMessage = 'Error al subir la foto o detalles del perro, hay valores repetidos';
+              return;
+            }
+    
+            const data = await response.json();
+            this.checkMessage = 'Foto y detalles del perro subidos correctamente';
+    
+            this.dogsDetails.breed = '';
+            this.dogsDetails.size = '';
+            this.dogsDetails.color = '';
+            this.dogsDetails.image = null;
+    
+            console.log('Respuesta del backend:', data);
+          } catch (error) {
+            console.error('Error en la petición al backend:', error);
+          }
+        }
+    }
+});
